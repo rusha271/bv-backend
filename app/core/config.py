6db63446516721b1bec4893c1dbe2754050dc17e
rusha271 <rushabhnakum440@gmail.com>
2025-10-11 12:13:40 +0530

    chnages in middelware

--- app/core/config.py
+++ app/core/config.py
@@ -7,7 +7,14 @@ class Settings(BaseSettings):
     JWT_SECRET_KEY: str = "....."
     JWT_ALGORITHM: str = "HS256"
     JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
-    FRONTEND_ORIGINS: List[str] = ["http://localhost:3000", "http://localhost:3001" , "http://localhost:3002"]
+    FRONTEND_ORIGINS: List[str] = [
+    "http://localhost:3000", 
+    "http://localhost:3001", 
+    "http://localhost:3002",
+    "https://bharmaspace.com",           # Add this
+    "https://www.bharmaspace.com",       # Add this
+    "https://api.bharmaspace.com"        # Add this (optional but recommended)
+    ]    
     ENV: str = "production"
     ENCRYPTION_KEY: str = "4cV9kH6sD2nP1yQ7wT5lJ3xA8oF2zE9rB1vL6mK7pN0="
     secret_key: str = "b9d4e7f0a1c2b3d5e6f7a8c9b0d1e2f3a4c5b6d7e8f9a0c1d2e3f4b5c6d7e8f"
